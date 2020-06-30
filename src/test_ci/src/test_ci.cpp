43c68551796f4f352d8ba5927e3d832599b0a9aa
Ibrahim Essam <ibrahim.essam1995@gmail.com>
2020-06-30 16:20:55 +0200

    test

--- src/test_ci/src/test_ci.cpp
+++ src/test_ci/src/test_ci.cpp
@@ -1,10 +1,9 @@
 #include <cstdio>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
 
-  printf("hello world test_ci package\n");
+  printf("hello world Yonohub test_ci package\n");
   return 0;
 }
